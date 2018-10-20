996078827cb742a9de31bef58feef2a6765c57f7
Nguyen Van Duy <nguyenvanduy.vtk33dlu@gmail.com>
2018-10-20 11:19:02 +0700

    remove unused constant variable

--- crypto/src/main/java/com/sap_press/rheinwerk_reader/crypto/Constant.java
+++ crypto/src/main/java/com/sap_press/rheinwerk_reader/crypto/Constant.java
@@ -3,7 +3,4 @@ package com.sap_press.rheinwerk_reader.crypto;
 public class Constant {
     public static final String IV = "581bcaf3f2d6281b5a2d2873caff7259";
     public static final String APP_KEY = "b6df5f479367881c42c7800534e7d8dddae6d186";
-    public static final String USER_KEY = "09164b431251450140595a4f16400140161958501d17540a1716550117555b5e46595c0a1242140d";
-    public static final String ENCRYPTED_CONTENT_KEY = "737c4ff5487d1f6062276208e1135bab";
-    public static final String X_CONTENT_KEY = "X-CONTENT-KEY";
 }
